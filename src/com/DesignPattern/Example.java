8058716ae07d55f83c21d29544c51ae10f6904f6
shabbirkhan <khanshabbir126@gmail.com>
2026-09-16 00:50:25 +0530

    breaking singleton

--- src/com/DesignPattern/Example.java
+++ src/com/DesignPattern/Example.java
@@ -1,8 +1,15 @@
 package com.DesignPattern;
 
-public class Example {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
 
-	public static void main(String[] args) {
+public class Example implements Serializable {
+
+	public static void main(String[] args) throws Exception {
 		
 		  SingletonPattern s1=SingletonPattern.getPattern();
 		  SingletonPattern s2=SingletonPattern.getPattern();
@@ -10,6 +17,25 @@ public class Example {
 		  System.out.println("S1:-" + s1 + " " + " S2:- "+ s2);
 		  
 		  System.out.println(s1==s2);
-		  System.out.println(s1.hashCode()==s2.hashCode());
+		  System.out.println(s1.hashCode());
+		  
+		  
+		 /* Constructor<SingletonPattern> constructor=SingletonPattern.class.getDeclaredConstructor();
+		  constructor.setAccessible(true);
+		  SingletonPattern singletonPattern=constructor.newInstance();
+		  
+		  System.out.println(singletonPattern.hashCode()); */
+		  
+		  // serilize 
+		   ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("xyz.ob"));
+		   oos.writeObject(s2);
+		   
+		   System.out.println("Serilize done");
+		   
+		   ObjectInputStream ois=new ObjectInputStream(new FileInputStream("xyz.ob"));
+		   SingletonPattern des2=(SingletonPattern) ois.readObject();
+		   
+		   System.out.println(des2.hashCode());
+		   System.out.println(s2.hashCode());
 		}
 }
--- src/com/DesignPattern/SingletonPattern.java
+++ src/com/DesignPattern/SingletonPattern.java
@@ -1,12 +1,17 @@
 package com.DesignPattern;
 
-public class SingletonPattern {
+import java.io.Serializable;
+
+public class SingletonPattern implements Serializable{
 
 	private static SingletonPattern singletonPattern;
 	
 	
 	private  SingletonPattern() {
     
+//		if (singletonPattern !=null) {
+//			throw new RuntimeException("You are trying to break singleton");
+//		}
 	}
 	
 	public static SingletonPattern getPattern()
@@ -20,6 +25,10 @@ public class SingletonPattern {
 		}
 		 
 		return singletonPattern;
-	}	
+	}
+//	 private Object readResolve() {
+//	        return singletonPattern;
+//	    }
+	
 }
 
